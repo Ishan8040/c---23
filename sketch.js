5bf435a7a1a54e0aee522df3ddcc8611b6d65d2b
Ishan8040 <73986485+Ishan8040@users.noreply.github.com>
2020-12-15 13:31:07 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,24 @@
+const Engine=Matter.Engine
+const Bodies=Matter.Bodies
+const World=Matter.World
+
+var engine,world;
+var box1;
+
+function setup() {
+  createCanvas(800,400);
+  engine= Engine.create()
+  world=engine.world
+box1=new Box(200,300,50,50);
+box2=new Box(240,100,50,100);
+ground=new Ground(200,height,400,20);
+}
+
+function draw() {
+  background("red"); 
+  Engine.update(engine);
+  box1.display();
+  box2.display();
+  ground.display();
+  drawSprites();
+}
